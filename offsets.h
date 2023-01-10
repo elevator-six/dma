ef9462895935bc91fbd806b7974305cbd4f221b9
5H4D3-uc <79592915+5H4D3-uc@users.noreply.github.com>
2023-01-10 10:26:59 -0500

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,15 +1,15 @@
 namespace offsets {
-        constexpr auto ref_def_ptr = 0x138ABB38;
-        constexpr auto name_array = 0x138CAF10;
+        constexpr auto ref_def_ptr = 0x138ACB38;
+        constexpr auto name_array = 0x138CBF20;
         constexpr auto name_array_pos = 0x5E70;
-        constexpr auto loot_ptr = 0xBADB12C;
-        constexpr auto camera_base = 0x13ECB810;
+        constexpr auto loot_ptr = 0xBADC12C;
+        constexpr auto camera_base = 0x13ECC810;
         constexpr auto camera_pos = 0x1F8;
         constexpr auto local_index = 0xB1D00;
         constexpr auto local_index_pos = 0x2D0;
-        constexpr auto game_mode = 0x100DABA8;
-        constexpr auto weapon_definitions = 0x1382EF90;
-        constexpr auto distribute = 0xBAA4998;
+        constexpr auto game_mode = 0x100DBDA8;
+        constexpr auto weapon_definitions = 0x1382FF90;
+        constexpr auto distribute = 0xBAA5998;
         constexpr auto visible_offset = 0xA80;
         constexpr auto visible = 0x28047A0;
         namespace player {
