4eb23a2d8be5f41d6e92f19a2fc6145ff94c1e66
5H4D3-uc <79592915+5H4D3-uc@users.noreply.github.com>
2023-01-16 17:17:07 -0500

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,29 +1,28 @@
 namespace offsets {
-        constexpr auto ref_def_ptr = 0x138ACB38;
-        constexpr auto name_array = 0x138CBF20;
-        constexpr auto name_array_pos = 0x5E70;
-        constexpr auto loot_ptr = 0xBADC12C;
-        constexpr auto camera_base = 0x13ECC810;
-        constexpr auto camera_pos = 0x1F8;
-        constexpr auto local_index = 0xB1D00;
-        constexpr auto local_index_pos = 0x2D0;
-        constexpr auto game_mode = 0x100DBDA8;
-        constexpr auto weapon_definitions = 0x1382FF90;
-        constexpr auto distribute = 0xBAA5998;
-        constexpr auto visible_offset = 0xA80;
-        constexpr auto visible = 0x28047A0;
-        namespace player {
-                constexpr auto size = 0x6980;
-                constexpr auto valid = 0x1301;
-                constexpr auto pos = 0xF70;
-                constexpr auto team = 0xFF8;
-                constexpr auto weapon_index = 0x1762;
-                constexpr auto dead_1 = 0x1E0A;
-                constexpr auto dead_2 = 0x1FD0;
-        }
-        namespace bone {
-                constexpr auto bone_base = 0xEBB30;
-                constexpr auto size = 0x180;
-                constexpr auto offset = 0xD8;
-        }
+    constexpr auto ref_def_ptr = 0x12F83B58;
+    constexpr auto name_array = 0x12FA2F40;
+    constexpr auto name_array_pos = 0x5E70;
+    constexpr auto loot_ptr = 0xB1B313C;
+    constexpr auto camera_base = 0x135A3810;
+    constexpr auto camera_pos = 0x1F8;
+    constexpr auto local_index = 0xE0DB0;
+    constexpr auto local_index_pos = 0x2D0;
+    constexpr auto game_mode = 0xF7B2DA8;
+    constexpr auto weapon_definitions = 0x12F05E70;
+    constexpr auto distribute = 0xB17C9A8;
+    constexpr auto visible_offset = 0xA80;
+    constexpr auto visible = 0x2260D50;
+    namespace player {
+        constexpr auto size = 0x6998;
+        constexpr auto valid = 0x10C1;
+        constexpr auto pos = 0x6528;
+        constexpr auto team = 0x1E8;
+        constexpr auto weapon_index = 0x10C2;
+        constexpr auto dead_1 = 0x6628;
+    }
+    namespace bone {
+        constexpr auto bone_base = 0xABB70;
+        constexpr auto size = 0x180;
+        constexpr auto offset = 0xD8;
+    }
 }
